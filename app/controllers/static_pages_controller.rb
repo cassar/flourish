3d7b7a8289b7dec21a5ab3006d3ad300a63309ce
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-07-09 15:45:55 +1000

    Set member generator job to start on page visit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+  after_action :start_member_generator
+
   def welcome
     @member_count = Member.count
+  end
+
+  private
+
+  def start_member_generator
+    MemberGeneratorJob.perform_now
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get welcome" do
+    MemberGeneratorJob.stubs(:perform_now).once
+
     get root_path
     assert_response :success
   end
