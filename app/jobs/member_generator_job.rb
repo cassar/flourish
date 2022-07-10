3e089b6107cff58b2c323892b4b1c0b8d75ea357
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-07-10 16:32:35 +1000

    Refactor member generator job

--- app/jobs/member_generator_job.rb
+++ app/jobs/member_generator_job.rb
@@ -3,19 +3,38 @@ class MemberGeneratorJob < ApplicationJob
 
   MAX_MEMBER_COUNT = 100
   MULTIPLIER = 100
+  CONTRIBUTION_RATIO = 1
 
   def perform(*args)
-    return Member.destroy_all if Member.count >= max_member_count
+    return Member.destroy_all if stop?
 
     Member.create!(contribution_amount: contribution_amount)
     self.class.set(wait: 3.seconds).perform_later
   end
 
-  def max_member_count
-    MAX_MEMBER_COUNT
+  def contribution_amount
+    return 0 unless contribute?
+
+    random_amount
   end
 
-  def contribution_amount
-    Integer rand() * MULTIPLIER
+  def stop?
+    member_count >= max_member_count
+  end
+
+  def contribute?
+    (member_count % CONTRIBUTION_RATIO).zero?
+  end
+
+  def random_amount
+    Integer(rand() * MULTIPLIER)
+  end
+
+  def member_count
+    MemberCountService.call
+  end
+
+  def max_member_count
+    MAX_MEMBER_COUNT
   end
 end
