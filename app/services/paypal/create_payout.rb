fdc1e7a0ed4b0b16990e0eedf276a71084d12503
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-08-09 20:29:41 +1000

    Add create payout paypal call

---
+++ app/services/paypal/create_payout.rb
@@ -0,0 +1,50 @@
+class Paypal::CreatePayout
+  class << self
+    def call
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def response
+      Excon.post(payouts_url,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{Paypal::AccessToken.call}"
+        },
+        body: {
+          sender_batch_header: {
+            sender_batch_id: "2014021801",
+            recipient_type: "EMAIL",
+            email_subject: "You have money!",
+            email_message: "You received a payment. Thanks for using our service!"
+          },
+          items: [
+            {
+              amount: {
+                value: "5",
+                currency: "AUD"
+              },
+              sender_item_id: "201403140001",
+              recipient_wallet: "PAYPAL",
+              receiver: "sb-9vror6710510@personal.example.com"
+            },
+            {
+              amount: {
+                value: "5",
+                currency: "AUD"
+              },
+              sender_item_id: "201403140002",
+              recipient_wallet: "PAYPAL",
+              receiver: "sb-otqu019747269@personal.example.com"
+            }
+          ]
+        }.to_json
+      )
+    end
+
+    def payouts_url
+      "#{ENV['paypal_api_v1_url']}/payments/payouts"
+    end
+  end
+end
