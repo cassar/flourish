4aa992faf61622123658bde7f69bd66603138418
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-08-13 21:02:18 +1000

    Add users table with email and digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_015557) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_103545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_10_015557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "contribution_amount", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
