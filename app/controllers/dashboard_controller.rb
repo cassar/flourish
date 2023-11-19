0afa69cb0d858202437fbb6783eae3840519fc7c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2023-11-19 14:01:35 +1000

    Don't start the member generator anymore

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
-  after_action :start_member_generator
-
   def welcome
     @member_count = ActiveMemberCountService.call
     @dividend_amount = Money.from_amount(DividendService::NextDividend::MINIMUM_DIVIDEND).format
@@ -8,11 +6,5 @@ class DashboardController < ApplicationController
     return unless user_signed_in?
 
     @member = current_user.member
-  end
-
-  private
-
-  def start_member_generator
-    MemberGeneratorJob.perform_now
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test 'should get dashboard' do
-    MemberGeneratorJob.stubs(:perform_now).once
-
     get root_path
     assert_response :success
   end
