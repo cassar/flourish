b7c7000b5830c328d84e224679879bb361ceb503
Luke Cassar <luke.cassar@uqconnect.edu.au>
2023-11-26 21:19:21 +1000

    Add association and tests to dividend

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,3 +1,4 @@
 class Dividend < ApplicationRecord
   belongs_to :distribution
+  belongs_to :member
 end
--- test/models/dividend_test.rb
+++ test/models/dividend_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DividendTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'belongs to distribution association' do
+    assert_equal distributions(:one), dividends(:one).distribution
+  end
+
+  test 'belongs to member association' do
+    assert_equal members(:active), dividends(:one).member
+  end
 end
