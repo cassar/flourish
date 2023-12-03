e890cd4972694bb272714d8d6a15ce7a9a2dc6f0
Luke Cassar <luke.cassar@uqconnect.edu.au>
2023-12-03 21:32:35 +1000

    Rebrand to Flourish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "universal_income_production"
+  # config.active_job.queue_name_prefix = "flourish_production"
 
   config.action_mailer.perform_caching = false
 
