5096d6eae8fd9c3ae34898259eaac618ac44ff20
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-01-06 14:35:37 +1000

    Remove next dividend information

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class DashboardController < ApplicationController
   def welcome
     @member_count = ActiveMemberCountService.call
-    @dividend_amount = Money.from_amount(DividendService::NextDividend::MINIMUM_DIVIDEND).format
     @total_pool = BankAccountService.balance_formatted
     return unless user_signed_in?
 
