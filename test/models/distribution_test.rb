83f9d618d78b8ea72ba9a97332524c727dfedcea
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-01-06 15:26:27 +1000

    Add dividends association to distribution

--- test/models/distribution_test.rb
+++ test/models/distribution_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class DistributionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Associations' do
+    assert_includes distributions(:one).dividends, dividends(:one)
+
+    distributions(:one).destroy!
+
+    assert_raises ActiveRecord::RecordNotFound do
+      dividends(:one).reload
+    end
+  end
 end
