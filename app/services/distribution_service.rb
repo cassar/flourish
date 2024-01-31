63c47ba87242b53a626ad77a21ff78ad64a0e682
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-01-31 22:12:11 +1000

    Update distribution service to check day

--- app/services/distribution_service.rb
+++ app/services/distribution_service.rb
@@ -1,19 +1,16 @@
 class DistributionService
-  attr_accessor :members, :total_amount
-
+  class NotTodayError < StandardError; end
   class NoMembersError < StandardError; end
   class NoMoneyError < StandardError; end
   class BelowMinimumDividendError < StandardError; end
 
-  def initialize(members:, total_amount:)
-    @members = members
-    @total_amount = total_amount
-  end
+  MINIMUM_DIVIDEND_IN_CENTS = 1000
 
   def call
+    raise NotTodayError unless distribution_is_today?
     raise NoMembersError if member_count.zero?
     raise NoMoneyError if total_amount.zero?
-    raise BelowMinimumDividendError if dividend_amount < minimum_dividend
+    raise BelowMinimumDividendError if dividend_amount_in_cents < minimum_dividend
 
     members.each do |member|
       create_dividend_and_send_notification(member)
@@ -28,10 +25,10 @@ class DistributionService
   end
 
   def distribution
-    @distribution ||= Distribution.create! dividend_amount:
+    @distribution ||= Distribution.create! dividend_amount: dividend_amount_in_cents
   end
 
-  def dividend_amount
+  def dividend_amount_in_cents
     total_amount / member_count
   end
 
@@ -40,6 +37,18 @@ class DistributionService
   end
 
   def minimum_dividend
-    DividendService::NextDividend::MINIMUM_DIVIDEND
+    MINIMUM_DIVIDEND_IN_CENTS
+  end
+
+  def members
+    ActiveMemberService.call
+  end
+
+  def total_amount
+    @total_amount ||= BankAccountService.balance
+  end
+
+  def distribution_is_today?
+    DistributionDayService.today?
   end
 end
--- test/services/distribution_service_test.rb
+++ test/services/distribution_service_test.rb
@@ -2,29 +2,49 @@ require 'test_helper'
 
 class DistributionTest < ActiveSupport::TestCase
   test 'creates dividends' do
+    DistributionDayService.stubs(:today?).returns(true)
+    ActiveMemberService.stubs(:call).returns([members(:one)])
+    BankAccountService.stubs(:balance).returns(DistributionService::MINIMUM_DIVIDEND_IN_CENTS)
+
     assert_difference 'Dividend.count' do
-      DistributionService.new(
-        members: [members(:one)],
-        total_amount: DividendService::NextDividend::MINIMUM_DIVIDEND
-      ).call
+      DistributionService.new.call
+    end
+  end
+
+  test 'fails when not the day for distributions' do
+    DistributionDayService.stubs(:today?).returns(false)
+
+    assert_raises DistributionService::NotTodayError do
+      DistributionService.new.call
     end
   end
 
   test 'fails when no members' do
+    DistributionDayService.stubs(:today?).returns(true)
+    ActiveMemberService.stubs(:call).returns([])
+
     assert_raises DistributionService::NoMembersError do
-      DistributionService.new(members: [], total_amount: 1).call
+      DistributionService.new.call
     end
   end
 
   test 'fails when no money' do
+    DistributionDayService.stubs(:today?).returns(true)
+    ActiveMemberService.stubs(:call).returns([members(:one)])
+    BankAccountService.stubs(:balance).returns(0)
+
     assert_raises DistributionService::NoMoneyError do
-      DistributionService.new(members: [members(:one)], total_amount: 0).call
+      DistributionService.new.call
     end
   end
 
   test 'fails when below minimum dividend' do
+    DistributionDayService.stubs(:today?).returns(true)
+    ActiveMemberService.stubs(:call).returns([members(:one)])
+    BankAccountService.stubs(:balance).returns(1)
+
     assert_raises DistributionService::BelowMinimumDividendError do
-      DistributionService.new(members: [members(:one)], total_amount: 1).call
+      DistributionService.new.call
     end
   end
 end
