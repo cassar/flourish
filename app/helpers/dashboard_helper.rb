c7660a0951b341f637672acdc7770494c6ce764c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-02-04 12:26:21 +1000

    Update dashboard helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,8 +1,5 @@
 module DashboardHelper
-  def next_friday_date_formatted
-    today = Time.zone.today
-    days_until_friday = (5 - today.wday + 7) % 7
-    next_friday = today + days_until_friday
-    next_friday.strftime('%a, %d %b %Y')
+  def next_distribution_date_formatted
+    DistributionDateService.next_date.strftime('%a, %d %b %Y')
   end
 end
--- test/helpers/dashboard_helper_test.rb
+++ test/helpers/dashboard_helper_test.rb
@@ -2,9 +2,8 @@ require 'test_helper'
 
 class DashboardHelperTest < ActionView::TestCase
   test 'should return next friday date' do
-    today = Date.new(2024, 1, 14)
-    Time.zone.stubs(:today).returns(today)
+    DistributionDateService.stubs(:next_date).returns(Date.parse('Friday 19th Jan 2024'))
     expected_date = 'Fri, 19 Jan 2024'
-    assert_equal expected_date, next_friday_date_formatted
+    assert_equal expected_date, next_distribution_date_formatted
   end
 end
