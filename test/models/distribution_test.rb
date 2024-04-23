3e5cdc490ff249fa543930f38409ab9d4327e6ba
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-04-23 21:40:55 +1000

    Add dividend amount formatted test

--- test/models/distribution_test.rb
+++ test/models/distribution_test.rb
@@ -10,4 +10,10 @@ class DistributionTest < ActiveSupport::TestCase
       dividends(:one).reload
     end
   end
+
+  test 'dividend amount formatted' do
+    assert_equal 500, distributions(:one).dividend_amount_in_base_units
+
+    assert_equal '$5.00', distributions(:one).dividend_amount_formatted
+  end
 end
