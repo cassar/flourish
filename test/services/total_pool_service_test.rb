183fc0a553048f206f73f82e4012663afbd9e04e
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-08-25 20:10:58 +1000

    Update total_pool_service_test.rb

--- test/services/total_pool_service_test.rb
+++ test/services/total_pool_service_test.rb
@@ -1,19 +1,23 @@
 require 'test_helper'
 
 class TotalPoolServiceTest < ActiveSupport::TestCase
-  setup do
+  test 'balance_in_base_units' do
     Contribution.stubs(:sum).returns(10_000)
 
     Distribution.stubs(:joins).returns(Distribution)
     Distribution.stubs(:merge).returns(Distribution)
     Distribution.stubs(:sum).returns(1_500)
-  end
 
-  test 'balance_in_base_units' do
     assert_equal 8_500, TotalPoolService.balance_in_base_units
   end
 
   test 'balance_formatted' do
+    Contribution.stubs(:sum).returns(10_000)
+
+    Distribution.stubs(:joins).returns(Distribution)
+    Distribution.stubs(:merge).returns(Distribution)
+    Distribution.stubs(:sum).returns(1_500)
+
     assert_equal '$85.00', TotalPoolService.balance_formatted
   end
 
