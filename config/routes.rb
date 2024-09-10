0191eab73c36c2e48f81391cdb4c6e4aaa81f47c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-10 15:19:58 +1000

    Rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :admin do
+    resources :dividends, only: [:show, :update]
+  end
+
   get :membership, to: 'memberships#show'
 
   resources :contributions, only: :index
+  resources :pay_outs, only: :index
+  resources :paypalmeids, only: [:edit, :update]
 
   resources :dividends, only: [:index, :show] do
     member { patch :pay_out, :recontribute } 
-  end
-
-  resources :pay_outs, only: [:index]
-  resources :paypalmeids, only: [:edit, :update]
-
-  namespace :admin do
-    resources :dividends, only: [:show, :update]
   end
 
   get :distributions, to: 'dashboard#distributions'
