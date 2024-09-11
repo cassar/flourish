41513a91b71d9199518958b604d3930522a732d1
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-11 11:03:30 +1000

    Add pool and next dividend to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   def dashboard
     @member_count = Member.active.count
     @total_pool = TotalPoolService.balance_formatted
+    @next_dividend_amount_formatted = DividendAmountService.new(
+      total_pool_in_base_units: TotalPoolService.balance_in_base_units,
+      member_count: @member_count
+    ).amount_formatted
     return unless user_signed_in?
 
     @member = current_user.member
