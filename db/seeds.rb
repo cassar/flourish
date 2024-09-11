6174d1d34b773c4f7f3dd97e81d5ff12bd81aeb6
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-11 10:35:10 +1000

    Add seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,27 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #
-User.create!(email: User::ADMIN_EMAIL, password: 'password')
+
+admin = User.new(email: User::ADMIN_EMAIL)
+admin.skip_confirmation_notification!
+admin.skip_reconfirmation!
+admin.confirm
+admin.save!
+
+1..50.times do |integer|
+  user = User.new(email: "user_#{integer}@email.com")
+  user.skip_confirmation_notification!
+  user.skip_reconfirmation!
+  user.confirm
+  user.save!
+end
+
+Contribution.create member: Member.first, amount_in_base_units: 20_000
+
+3.times do |integer|
+  distribution = Distribution.create(dividend_amount_in_base_units: 1_000)
+
+  Member.take(3).each do |member|
+    Dividend.create member:, distribution:
+  end
+end
