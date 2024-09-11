ca3aad4cc9571025c344e6939c0bb038afd72268
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-11 14:14:20 +1000

    Refactor admin dividends controller test

--- test/controllers/admin/dividends_controller_test.rb
+++ test/controllers/admin/dividends_controller_test.rb
@@ -2,52 +2,54 @@ require 'test_helper'
 
 module Admin
   class DividendsControllerTest < ActionDispatch::IntegrationTest
-    setup do
-      @dividend = dividends(:one)
-      @user = users(:one)
-      sign_in @user
-    end
-
     test 'should redirect to sign in when not authenticated for show' do
-      sign_out @user
-      get admin_dividend_path(@dividend)
+      get admin_dividend_path(dividends(:one))
       assert_redirected_to new_user_session_path
     end
 
     test 'should redirect to sign in when not authorized for show' do
-      User.any_instance.stubs(:admin?).returns(false)
-      get admin_dividend_path(@dividend)
+      assert_not users(:one).admin?
+      sign_in users(:one)
+
+      get admin_dividend_path(dividends(:one))
       assert_redirected_to dividends_path
     end
 
     test 'should get show' do
-      User.any_instance.stubs(:admin?).returns(true)
-      get admin_dividend_path(@dividend)
+      assert users(:admin).admin?
+      sign_in users(:admin)
+
+      get admin_dividend_path(dividends(:one))
       assert_response :success
     end
 
     test 'should redirect to sign in when not authenticated for update' do
-      sign_out @user
-      patch admin_dividend_path(@dividend)
+      patch admin_dividend_path(dividends(:one))
       assert_redirected_to new_user_session_path
     end
 
     test 'should redirect to sign in when not authorized for update' do
-      assert_nil @dividend.receipt
-      User.any_instance.stubs(:admin?).returns(false)
-      patch admin_dividend_path(@dividend), params: { dividend: { receipt: 'new_receipt' } }
+      assert_not users(:one).admin?
+      sign_in users(:one)
+
+      assert_nil dividends(:one).receipt
+      patch admin_dividend_path(dividends(:one)), params: { dividend: { receipt: 'new_receipt' } }
+
       assert_redirected_to dividends_path
       assert_equal "You don't have access.", flash[:alert]
-      assert_nil @dividend.receipt
+      assert_nil dividends(:one).receipt
     end
 
     test 'should patch update' do
-      assert_nil @dividend.receipt
-      User.any_instance.stubs(:admin?).returns(true)
-      patch admin_dividend_path(@dividend), params: { dividend: { receipt: 'new_receipt' } }
+      assert users(:admin).admin?
+      sign_in users(:admin)
+
+      assert_nil dividends(:one).receipt
+      patch admin_dividend_path(dividends(:one)), params: { dividend: { receipt: 'new_receipt' } }
+
       assert_response :success
       assert_equal 'Receipt updated successfully.', flash[:success]
-      assert_equal 'new_receipt', @dividend.reload.receipt
+      assert_equal 'new_receipt', dividends(:one).reload.receipt
     end
   end
 end
