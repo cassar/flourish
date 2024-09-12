b4580df9636605f17bad732628e502b5079c8786
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-12 10:35:58 +1000

    Add user count to admin views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ module Admin
 
     def index
       @users = User.order(Arel.sql('confirmed_at NULLS LAST'), Arel.sql('last_sign_in_at DESC NULLS LAST'))
+      @total_user_count = User.count
     end
 
     private
