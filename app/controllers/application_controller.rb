306c6a9df271612766c07f6685e0a44ba8d7cf87
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-18 15:57:28 +1000

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   private
 
   def repel_bot_with_invisible_captcha
-    Honeybadger.notify("A bot was repeled with invisible_captcha")
+    Honeybadger.notify('A bot was repeled with invisible_captcha')
     head :ok
   end
 
   def repel_bot_with_voight_kampff
     return unless request.bot?
 
-    Honeybadger.notify("A bot was repeled with voight_kampff")
+    Honeybadger.notify('A bot was repeled with voight_kampff')
     head :ok
   end
 end
