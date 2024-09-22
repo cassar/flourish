64b82bfc70ee6c03861ebfd5a14d5e2bf7659138
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-22 11:11:01 +1000

    Set time zone as Brisbane

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Flourish
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Brisbane'
   end
 end
