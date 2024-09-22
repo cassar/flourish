8bb2592d96bbef28603fb6c7c86cd864fdf811eb
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-22 18:19:04 +1000

    Remove redundant stub

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get dashboard' do
-    TotalPoolService.stubs(:balance_formatted).returns('$133.00')
-
     get root_path
     assert_response :success
   end
