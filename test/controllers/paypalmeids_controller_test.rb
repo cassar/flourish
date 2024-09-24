d0a817572cf44fde3f1d60a2a7fb2d50b2ed97e2
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-24 15:27:41 +1000

    Refactor paypalmeid controller

--- test/controllers/paypalmeids_controller_test.rb
+++ test/controllers/paypalmeids_controller_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
 class PaypalmeidsControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = users(:one)
-    sign_in @user
-    @member = @user.member
-  end
-
   test 'should redirect to sign in when not authenticated for edit' do
-    sign_out @user
-    get edit_paypalmeid_path(@member)
+    get edit_paypalmeid_path(members(:one))
+
     assert_redirected_to new_user_session_path
   end
 
   test 'should get edit' do
-    get edit_paypalmeid_path(@member)
+    sign_in users(:one)
+
+    get edit_paypalmeid_path(members(:one))
+
     assert_response :success
   end
 
   test 'should redirect to sign in when not authenticated for update' do
-    sign_out @user
-    patch paypalmeid_path(@member), params: { member: { paypalmeid: 'new_paypalmeid' } }
+    patch paypalmeid_path(members(:one)), params: { member: { paypalmeid: 'new_paypalmeid' } }
+
     assert_redirected_to new_user_session_path
   end
 
   test 'should update paypalmeid' do
-    patch paypalmeid_path(@member), params: { member: { paypalmeid: 'new_paypalmeid' } }
+    sign_in users(:one)
+
+    patch paypalmeid_path(members(:one)), params: { member: { paypalmeid: 'new_paypalmeid' } }
+
     assert_redirected_to membership_path
     assert_equal 'PayPal.Me ID updated successfully.', flash[:success]
-    @member.reload
-    assert_equal 'new_paypalmeid', @member.paypalmeid
+    members(:one).reload
+    assert_equal 'new_paypalmeid', members(:one).paypalmeid
   end
 end
