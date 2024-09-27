c7453ec2452fdf49002f9b38f99d17550abe404c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-27 15:58:50 +1000

    Add distribution status helper

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -5,4 +5,10 @@ module DistributionsHelper
       status_counts[aggregate_status] += count
     end
   end
+
+  def distribution_status(dividend_statuses:)
+    return 'Settled' if dividend_statuses.none?('issued')
+
+    'Awaiting action from all members'
+  end
 end
--- test/helpers/distributions_helper_test.rb
+++ test/helpers/distributions_helper_test.rb
@@ -15,4 +15,14 @@ class DistributionsHelperTest < ActionView::TestCase
 
     assert_equal expected_aggregate_status_counts, aggregate_status_counts(disaggregate_status_counts)
   end
+
+  test 'distribution_status when settled' do
+    dividend_statuses = %w[paid_out manually_recontributed]
+    assert_equal 'Settled', distribution_status(dividend_statuses:)
+  end
+
+  test 'distribution_status when not settled' do
+    dividend_statuses = %w[paid_out issued]
+    assert_equal 'Awaiting action from all members', distribution_status(dividend_statuses:)
+  end
 end
