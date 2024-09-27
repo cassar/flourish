f923f18c3b92818d1a437cf1cf525196c29d2a23
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-27 16:15:35 +1000

    Spread out home action a bit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    @member_count = Member.active.count
-    @total_pool = TotalPoolService.balance_formatted
     @last_distribution = Distribution.last
     @unsettled = @last_distribution.nil? || @last_distribution.dividends.issued.any?
+
+    @member_count = Member.active.count
+    @total_pool = TotalPoolService.balance_formatted
     @next_dividend_amount_formatted = DividendAmountService.new(
       total_pool_in_base_units: TotalPoolService.balance_in_base_units,
       member_count: @member_count
     ).amount_formatted
+
     return unless user_signed_in?
 
     @member = current_user.member
