e1d6c44070dce0eb6404b0c7f3bf378a4b5afe40
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-27 14:12:36 +1000

    Extend paypalme_url to exclude amount

--- app/helpers/admin/pay_outs_helper.rb
+++ app/helpers/admin/pay_outs_helper.rb
@@ -1,8 +1,9 @@
 module Admin
   module PayOutsHelper
-    def paypalme_url(paypalmeid:, amount_in_base_units:)
-      amount = Money.from_cents(amount_in_base_units).format(symbol: false).gsub(' ', '')
+    def paypalme_url(paypalmeid:, amount_in_base_units: nil)
+      return "https://paypal.me/#{paypalmeid}" if amount_in_base_units.nil?
 
+      amount = Money.from_cents(amount_in_base_units).format(symbol: false).gsub(' ', '')
       "https://paypal.me/#{paypalmeid}/#{amount}"
     end
   end
--- test/helpers/admin/pay_outs_helper_test.rb
+++ test/helpers/admin/pay_outs_helper_test.rb
@@ -2,11 +2,20 @@ require 'test_helper'
 
 module Admin
   class PayOutsHelperTest < ActionView::TestCase
-    test 'paypalme_url' do
+    test 'paypalme_url with amount in base units' do
       expected = 'https://paypal.me/mypaypalmeid/5.50AUD'
       actual = paypalme_url(
         paypalmeid: 'mypaypalmeid',
         amount_in_base_units: 550
+      )
+
+      assert_equal expected, actual
+    end
+
+    test 'paypalme_url without amount in base units' do
+      expected = 'https://paypal.me/mypaypalmeid'
+      actual = paypalme_url(
+        paypalmeid: 'mypaypalmeid'
       )
 
       assert_equal expected, actual
