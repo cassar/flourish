d9b51044f748a92c52b54ebaa6b8a0d1474332bf
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-27 14:15:19 +1000

    Remove paypalmeid_emoji

--- app/helpers/admin/members_helper.rb
+++ app/helpers/admin/members_helper.rb
@@ -1,11 +1,4 @@
 module Admin
   module MembersHelper
-    def paypalmeid_emoji(paypalmeid)
-      if paypalmeid.present?
-        '💰'
-      else
-        '🚫'
-      end
-    end
   end
 end
--- test/helpers/admin/members_helper_test.rb
+++ test/helpers/admin/members_helper_test.rb
@@ -2,12 +2,5 @@ require 'test_helper'
 
 module Admin
   class MembersHelperTest < ActionView::TestCase
-    test 'paypalmeid present' do
-      assert_equal '💰', paypalmeid_emoji('a_paypalme_id')
-    end
-
-    test 'paypalmeid not present' do
-      assert_equal '🚫', paypalmeid_emoji(nil)
-    end
   end
 end
