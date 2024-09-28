4828b87a7cc4d10873c5d4ee88f78d9ef7846a4f
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-28 11:44:40 +1000

    Extend distribution status helper

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -1,7 +1,9 @@
 module DistributionsHelper
   def distribution_status(dividend_statuses:)
-    return 'All dividends recontributed or paid out' if dividend_statuses.none?('issued')
+    return 'All dividends recontributed or paid out to members' if dividend_statuses.none?('issued')
 
-    'Dividends issued to members'
+    return 'Dividends issued to members' if dividend_statuses.all?('issued')
+
+    'Awaiting action from members'
   end
 end
--- test/helpers/distributions_helper_test.rb
+++ test/helpers/distributions_helper_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class DistributionsHelperTest < ActionView::TestCase
-  test 'distribution_status when settled' do
+  test 'distribution_status when all actioned' do
     dividend_statuses = %w[paid_out manually_recontributed]
-    assert_equal 'All dividends recontributed or paid out', distribution_status(dividend_statuses:)
+    assert_equal 'All dividends recontributed or paid out to members', distribution_status(dividend_statuses:)
   end
 
-  test 'distribution_status when not settled' do
-    dividend_statuses = %w[paid_out issued]
+  test 'distribution_status when all issued' do
+    dividend_statuses = %w[issued issued]
     assert_equal 'Dividends issued to members', distribution_status(dividend_statuses:)
+  end
+
+  test 'distribution_status when some actioned' do
+    dividend_statuses = %w[issued manually_recontributed]
+    assert_equal 'Awaiting action from members', distribution_status(dividend_statuses:)
   end
 end
