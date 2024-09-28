b64123a12749137e837c3327402c4e7081372bdf
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-28 11:37:17 +1000

    Refactor home page and tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
-    @last_distribution = Distribution.last
-    @unsettled = @last_distribution.nil? || @last_distribution.dividends.issued.any?
+    @last_distribution = Distribution.last || Distribution.new
+    if (@unsettled = @last_distribution.dividends.issued.any?)
+      @last_distribution_dividends = @last_distribution.dividends
+    else
+      distribution_preview
+    end
+    authenticated_user
+  end
 
+  private
+
+  def distribution_preview
     @member_count = Member.active.count
     @total_pool = TotalPoolService.balance_formatted
     @next_dividend_amount_formatted = DividendAmountService.new(
       total_pool_in_base_units: TotalPoolService.balance_in_base_units,
       member_count: @member_count
     ).amount_formatted
+  end
 
+  def authenticated_user
     return unless user_signed_in?
 
     @member = current_user.member
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home when previous distribution' do
-    assert distributions.any?
+  test 'should get home when previous distribution with issued dividends' do
+    assert Distribution.last
+    assert Distribution.last.dividends.issued.any?
 
     get root_path
     assert_response :success
   end
 
-  test 'should get home when no previous distribution' do
+  test 'should get home when last distribution is settled' do
     Distribution.destroy_all
+
+    get root_path
+    assert_response :success
+  end
+
+  test 'should get home when user authenticated' do
+    sign_in users(:one)
 
     get root_path
     assert_response :success
