10aa221d609e9637465e45644d3616f547970a8a
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-30 12:33:51 +1000

    Add name column to distributions

---
+++ db/migrate/20240930022811_name_distribution.rb
@@ -0,0 +1,5 @@
+class NameDistribution < ActiveRecord::Migration[8.0]
+  def change
+    add_column :distributions, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_25_053501) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_30_022811) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "contributions", force: :cascade do |t|
     t.integer "amount_in_base_units"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_25_053501) do
     t.integer "dividend_amount_in_base_units"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "dividends", force: :cascade do |t|
