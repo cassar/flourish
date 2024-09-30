c21008aa72398d605dc04dcc03affd006ec2c257
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-30 14:06:57 +1000

    Make settlement text more succinct

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -1,6 +1,6 @@
 module DistributionsHelper
   def distribution_status(dividend_statuses:)
-    return 'All dividends recontributed or paid out to members' if dividend_statuses.none?('issued')
+    return 'Settled' if dividend_statuses.none?('issued')
 
     return 'Dividends issued to members' if dividend_statuses.all?('issued')
 
--- test/helpers/distributions_helper_test.rb
+++ test/helpers/distributions_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DistributionsHelperTest < ActionView::TestCase
   test 'distribution_status when all actioned' do
     dividend_statuses = %w[paid_out manually_recontributed]
-    assert_equal 'All dividends recontributed or paid out to members', distribution_status(dividend_statuses:)
+    assert_equal 'Settled', distribution_status(dividend_statuses:)
   end
 
   test 'distribution_status when all issued' do
