13a86a733dc95a5771516b0bcce3b270d602f830
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-09-30 14:00:12 +1000

    Refactor distribution service test

--- test/services/distribution_service_test.rb
+++ test/services/distribution_service_test.rb
@@ -1,24 +1,23 @@
 require 'test_helper'
 
 class DistributionServiceTest < ActiveSupport::TestCase
-  test 'creates a single distribution with the given divindend amount' do
-    members = [members(:one), members(:two)]
-    dividend_amount_in_base_units = 1000
-
+  test 'creates a single distribution' do
     assert_difference 'Distribution.count', 1 do
       DistributionService.new(
-        members:,
-        dividend_amount_in_base_units:
+        members: [members(:one)],
+        dividend_amount_in_base_units: 1000
       ).call
     end
-
-    assert_equal dividend_amount_in_base_units, Distribution.last.dividend_amount_in_base_units
   end
 
-  test 'all dependent services respond' do
-    assert DistributionService.new(
+  test 'creates a distribution with the given dividend amount' do
+    dividend_amount_in_base_units = 1000
+
+    DistributionService.new(
       members: [members(:one)],
-      dividend_amount_in_base_units: 0
+      dividend_amount_in_base_units:
     ).call
+
+    assert_equal dividend_amount_in_base_units, Distribution.last.dividend_amount_in_base_units
   end
 end
