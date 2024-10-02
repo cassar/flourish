c9517c6a5be853221bce0f2eb95fbb47ccfa074c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-02 17:14:56 +1000

    Add next date test coverage

--- test/services/distribution_date_service_test.rb
+++ test/services/distribution_date_service_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class DistributionDateServiceTest < ActiveSupport::TestCase
+  test 'next date' do
+    assert_equal Date, DistributionDateService.next_date.class
+  end
+
   test 'distribution is not today' do
     Time.zone.stubs(:today).returns(Date.parse('Monday 29th Jan 2024'))
     DistributionDateService.stubs(:next_date).returns(Date.parse('Tuesday 30th Jan 2024'))
