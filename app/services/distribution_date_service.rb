844e119ddd37986053c88bb3944f5f254c508b22
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-02 17:14:31 +1000

    Refactor distribution date service

--- app/services/distribution_date_service.rb
+++ app/services/distribution_date_service.rb
@@ -1,11 +1,13 @@
 class DistributionDateService
   DAY_OF_THE_WEEK = 'Friday'.freeze
 
-  def self.next_date
-    Date.parse(DAY_OF_THE_WEEK)
-  end
+  class << self
+    def next_date
+      Date.parse(DAY_OF_THE_WEEK)
+    end
 
-  def self.today?
-    Time.zone.today.eql? next_date
+    def today?
+      Time.zone.today.eql? next_date
+    end
   end
 end
