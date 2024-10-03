b4dacd71adcd015e608180b379b738d3ef902708
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-03 17:15:13 +1000

    Create distribution preview date service

---
+++ app/services/distribution_preview_date_service.rb
@@ -0,0 +1,13 @@
+class DistributionPreviewDateService
+  DAY_OF_THE_WEEK = 'Wednesday'.freeze
+
+  class << self
+    def next_date
+      Date.parse(DAY_OF_THE_WEEK)
+    end
+
+    def today?
+      Time.zone.today.eql? next_date
+    end
+  end
+end
