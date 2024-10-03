53c8b8cfc9bcee592167e377a371e4f4548ff12f
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-03 17:13:48 +1000

    Use User.active in Member.active

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,9 +7,7 @@ class Member < ApplicationRecord
   validates :paypalmeid, uniqueness: { allow_nil: true, case_sensitive: false }
 
   scope :active, lambda {
-    joins(:user)
-      .where.not(users: { email: User::ADMIN_EMAIL })
-      .where.not(users: { last_sign_in_at: nil })
+    joins(:user).merge(User.active)
   }
 
   scope :inactive, lambda {
