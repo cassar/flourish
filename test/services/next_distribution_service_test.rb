7ec50e04782999de88760916958938dfa5b69ca3
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-03 15:30:49 +1000

    Create next_distribution_service_test.rb

---
+++ test/services/next_distribution_service_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class NextDistributionServiceTest < ActiveSupport::TestCase
+  test 'distribute!' do
+    assert_instance_of Array, NextDistributionService.distribute!
+  end
+
+  test 'members' do
+    assert NextDistributionService.members
+  end
+
+  test 'member_count' do
+    result = NextDistributionService.member_count
+
+    assert_instance_of Integer, result
+  end
+
+  test 'name' do
+    result = NextDistributionService.name
+
+    assert_instance_of String, result
+  end
+
+  test 'date_formatted' do
+    result = NextDistributionService.date_formatted
+
+    assert_instance_of String, result
+  end
+
+  test 'today?' do
+    assert_not_nil NextDistributionService.today?
+  end
+
+  test 'total_pool_formatted' do
+    result = NextDistributionService.total_pool_formatted
+
+    assert_instance_of String, result
+  end
+
+  test 'dividend_amount_formatted' do
+    result = NextDistributionService.dividend_amount_formatted
+
+    assert_instance_of String, result
+  end
+
+  test 'dividend_amount_in_base_units' do
+    result = NextDistributionService.dividend_amount_in_base_units
+
+    assert_instance_of Integer, result
+  end
+end
