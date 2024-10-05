3e22ea67c76e90e024adfc76e11450c1b8569ad7
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-05 13:45:10 +1000

    Remove unused attribute in admin notification

--- test/mailers/previews/admin_notification_mailer_preview.rb
+++ test/mailers/previews/admin_notification_mailer_preview.rb
@@ -2,7 +2,7 @@
 class AdminNotificationMailerPreview < ActionMailer::Preview
   def pay_out_requested
     user = User.new(email: 'email@example.com')
-    member = Member.new(paypalmeid: 'my_paypalmeid', user:)
+    member = Member.new(user:)
     dividend = Dividend.new(id: 1, member:)
     AdminNotificationMailer.with(dividend:).pay_out_requested
   end
