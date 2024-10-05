d7c6626f2180152186b90a4327a5bcee0f747582
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-05 13:40:48 +1000

    Remove old paypalmeid endpoints in members

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,25 +4,4 @@ class MembershipsController < ApplicationController
   def show
     @member = current_user.member
   end
-
-  def edit_paypalmeid
-    @member = current_user.member
-  end
-
-  def update
-    @member = current_user.member
-    if @member.update(member_params)
-      flash[:success] = I18n.t('controllers.memberships.update.success')
-      redirect_to membership_path
-    else
-      flash.now[:error] = @member.errors.full_messages.to_sentence
-      render :edit_paypalmeid
-    end
-  end
-
-  private
-
-  def member_params
-    params.require(:member).permit(:paypalmeid)
-  end
 end
