2ea4c33fccf5811cfba81bf45184c974b60376a7
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-05 13:47:16 +1000

    Remove unused attribute in notification previews

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -2,14 +2,14 @@
 class NotificationMailerPreview < ActionMailer::Preview
   def contribution_received
     user = User.new(email: 'email@example.com')
-    member = Member.new(paypalmeid: 'my_paypalmeid', user:)
+    member = Member.new(user:)
     contribution = Contribution.new(amount_in_base_units: 500, transaction_identifier: 'teuharub239', member:)
     NotificationMailer.with(contribution:).contribution_received
   end
 
   def dividend_received
     user = User.new(email: 'email@example.com')
-    member = Member.new(paypalmeid: 'my_paypalmeid', user:)
+    member = Member.new(user:)
     distribution = Distribution.new(dividend_amount_in_base_units: 400)
     dividend = Dividend.new(id: 1, member:, distribution:)
     NotificationMailer.with(dividend:).dividend_received
@@ -17,7 +17,7 @@ class NotificationMailerPreview < ActionMailer::Preview
 
   def dividend_paid_out
     user = User.new(email: 'email@example.com')
-    member = Member.new(paypalmeid: 'my_paypalmeid', user:)
+    member = Member.new(user:)
     distribution = Distribution.new(dividend_amount_in_base_units: 400)
     dividend = Dividend.new(id: 1, member:, distribution:, receipt: '172receipt_no38a')
     NotificationMailer.with(dividend:).dividend_paid_out
@@ -25,7 +25,7 @@ class NotificationMailerPreview < ActionMailer::Preview
 
   def dividend_automatically_recontributed
     user = User.new(email: 'email@example.com')
-    member = Member.new(paypalmeid: 'my_paypalmeid', user:)
+    member = Member.new(user:)
     distribution = Distribution.new(dividend_amount_in_base_units: 400)
     dividend = Dividend.new(id: 1, member:, distribution:, receipt: '172receipt_no38a')
     NotificationMailer.with(dividend:).dividend_automatically_recontributed
