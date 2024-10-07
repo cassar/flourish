9d4b868232f4016102c91b7bf09e6dd7e73c5a27
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-07 19:40:50 +1000

    Improve total pool service test

--- test/services/total_pool_service_test.rb
+++ test/services/total_pool_service_test.rb
@@ -11,17 +11,17 @@ class TotalPoolServiceTest < ActiveSupport::TestCase
     assert_equal 8_500, TotalPoolService.balance_in_base_units
   end
 
-  test 'balance_formatted' do
-    Contribution.stubs(:sum).returns(10_000)
-
-    Distribution.stubs(:joins).returns(Distribution)
-    Distribution.stubs(:merge).returns(Distribution)
-    Distribution.stubs(:sum).returns(1_500)
-
-    assert_equal '$85.00 AUD', TotalPoolService.balance_formatted
+  test 'balance in base units integration' do
+    assert_instance_of Integer, TotalPoolService.balance_in_base_units
   end
 
-  test 'integration' do
+  test 'balance_formatted' do
+    TotalPoolService.stubs(:balance_in_base_units).returns(10_000)
+
+    assert_equal '$100.00 AUD', TotalPoolService.balance_formatted
+  end
+
+  test 'balance formatted integration' do
     assert_instance_of String, TotalPoolService.balance_formatted
   end
 end
