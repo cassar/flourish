dfce211d4b444d39a53a97e64c219b3df3531a92
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-10-08 13:57:20 +1000

    Add amount_formatted method to Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+  def amount_formatted
+    Money.from_cents(amount_in_base_units).format
+  end
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'amount_formatted' do
+    assert_equal '$9.98 AUD', Expense.new(amount_in_base_units: 998).amount_formatted
+  end
 end
