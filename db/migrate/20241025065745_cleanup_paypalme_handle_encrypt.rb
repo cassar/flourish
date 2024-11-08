66c02200c168fb5e44f02486ac7a9048698fd386
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-08 12:32:14 +1000

    Remove old migrations

--- db/migrate/20241025065745_cleanup_paypalme_handle_encrypt.rb
+++
@@ -1,6 +0,0 @@
-class CleanupPaypalmeHandleEncrypt < ActiveRecord::Migration[8.1]
-  def change
-    rename_column :members, :paypalme_handle, :paypalme_handle_old
-    rename_column :members, :paypalme_handle_new, :paypalme_handle
-  end
-end
