c533178ff362bf3bdfb81cbe80d2769222d4abc1
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-13 14:25:12 +1000

    Remove old migrations

--- db/migrate/20241107075447_add_member_currency.rb
+++
@@ -1,5 +0,0 @@
-class AddMemberCurrency < ActiveRecord::Migration[8.1]
-  def change
-    add_column :members, :currency, :string, default: 'AUD'
-  end
-end
--- db/migrate/20241108023453_add_currency_to_contributions.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrencyToContributions < ActiveRecord::Migration[8.1]
-  def change
-    add_column :contributions, :currency, :string, default: 'AUD'
-  end
-end
