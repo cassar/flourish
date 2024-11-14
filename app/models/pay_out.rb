96b7189bd8a9ac89d399bfac743428876da07581
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-14 20:13:35 +1000

    Add base unit validations to pay out model

--- app/models/pay_out.rb
+++ app/models/pay_out.rb
@@ -1,6 +1,8 @@
 class PayOut < ApplicationRecord
   belongs_to :dividend
 
+  validates :amount_in_base_units, numericality: { only_integer: true, greater_than: 0 }
+  validates :fees_in_base_units, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :transaction_identifier, uniqueness: true
 
   include CurrencyValidator
--- test/models/pay_out_test.rb
+++ test/models/pay_out_test.rb
@@ -8,12 +8,40 @@ class PayOutTest < ActiveSupport::TestCase
     assert_equal dividends(:pay_out_complete), pay_outs(:pay_out_complete).dividend
   end
 
+  test 'amount in base units non positive integers' do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      pay_outs(:pay_out_complete).update!(
+        amount_in_base_units: 0
+      )
+    end
+
+    expected_error = 'Validation failed: Amount in base units must be greater than 0'
+
+    assert_equal expected_error, error.message
+  end
+
+  test 'fees in base units negative integers' do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      pay_outs(:pay_out_complete).update!(
+        fees_in_base_units: -1
+      )
+    end
+
+    expected_error = 'Validation failed: Fees in base units must be greater than or equal to 0'
+
+    assert_equal expected_error, error.message
+  end
+
+  test 'fees in base units default value' do
+    assert_equal 0, PayOut.new.fees_in_base_units
+  end
+
   test 'transaction identifier validation' do
     error = assert_raises ActiveRecord::RecordInvalid do
       PayOut.new(
         amount_in_base_units: 500,
         transaction_identifier: pay_outs(:pay_out_complete).transaction_identifier
-      ).save! 
+      ).save!
     end
 
     assert_match(/Transaction ID has already been taken/, error.message)
