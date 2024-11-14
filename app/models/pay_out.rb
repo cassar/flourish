783cc594e1d5d295ac0b89bc862542e1819bbcb1
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-14 16:50:25 +1000

    Add unique validation to pay out transaction id

--- app/models/pay_out.rb
+++ app/models/pay_out.rb
@@ -1,5 +1,7 @@
 class PayOut < ApplicationRecord
   belongs_to :dividend
 
+  validates :transaction_identifier, uniqueness: true
+
   include CurrencyValidator
 end
--- test/models/pay_out_test.rb
+++ test/models/pay_out_test.rb
@@ -8,6 +8,17 @@ class PayOutTest < ActiveSupport::TestCase
     assert_equal dividends(:pay_out_complete), pay_outs(:pay_out_complete).dividend
   end
 
+  test 'transaction identifier validation' do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      PayOut.new(
+        amount_in_base_units: 500,
+        transaction_identifier: pay_outs(:pay_out_complete).transaction_identifier
+      ).save! 
+    end
+
+    assert_match(/Transaction ID has already been taken/, error.message)
+  end
+
   test 'currency inclusion validation' do
     @currency_capable = pay_outs(:pay_out_complete)
     currency_inclusion_validation
