2f111be0034d1a48ece7b32e9a221fa8c700b9f6
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-15 18:08:01 +1000

    Use default amount for distributions views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @last_distribution = Distribution.last || Distribution.new
     if (@unsettled = @last_distribution.dividends.issued.any?)
-      @last_amount = @last_distribution.amounts.first
+      @last_amount = @last_distribution.default_amount
       @last_distribution_dividends = @last_distribution.dividends
     else
       @next_distribution = NextDistributionService
