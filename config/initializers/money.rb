5971ee347bc485b5aa807549bf227e892f9ea7a6
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-11-20 16:14:16 +1000

    Update currencies on initialise

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,5 +1,7 @@
+require_relative '../../app/services/currency_updater_service.rb'
 Money.default_bank = EuCentralBank.new
 Money.default_currency = Money::Currency.new("AUD")
 Money.locale_backend = :i18n
 Money.rounding_mode = BigDecimal::ROUND_HALF_EVEN
 Money.default_formatting_rules = { with_currency: true }
+CurrencyUpdaterService.call
