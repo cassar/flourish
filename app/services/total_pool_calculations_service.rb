94336cde37255ee14bdfb1196e474cd9d28f6247
Luke Cassar <luke.cassar@uqconnect.edu.au>
2024-12-09 20:47:58 +1000

    Add new methods to calculations service

--- app/services/total_pool_calculations_service.rb
+++ app/services/total_pool_calculations_service.rb
@@ -18,5 +18,19 @@ class TotalPoolCalculationsService
         .group(:currency)
         .sum(:amount_in_base_units)
     end
+
+    def total_recontributions_by_currency
+      Amount.joins(:dividends)
+            .merge(Dividend.recontributed)
+            .group(:currency)
+            .sum(:amount_in_base_units)
+    end
+
+    def total_dividends_by_currency
+      Amount.joins(:dividends)
+            .merge(Dividend.all)
+            .group(:currency)
+            .sum(:amount_in_base_units)
+    end
   end
 end
--- test/services/total_pool_calculations_service_test.rb
+++ test/services/total_pool_calculations_service_test.rb
@@ -12,4 +12,12 @@ class TotalPoolCalculationsServiceTest < ActiveSupport::TestCase
   test 'total_paid_out_by_currency' do
     assert_instance_of Hash, TotalPoolCalculationsService.total_paid_out_by_currency
   end
+
+  test 'total_recontributions_by_currency' do
+    assert_instance_of Hash, TotalPoolCalculationsService.total_recontributions_by_currency
+  end
+
+  test 'total_dividends_by_currency' do
+    assert_instance_of Hash, TotalPoolCalculationsService.total_dividends_by_currency
+  end
 end
