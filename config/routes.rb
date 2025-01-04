a8779697d410147864e496a50b65ce1ce6cb1f3c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-01-04 15:24:59 +1000

    Move routes around

--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,29 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   namespace :admin do
+    resources :distributions, only: :index
+    resources :users, only: :destroy
     resources :dividends, only: [] do
       resources :pay_outs, only: [:new, :create] do
         collection { get :preview }
       end
     end
-    resources :distributions, only: :index
     resources :members, only: [] do
+      resources :contributions, only: [:new, :create] do
+        collection { get :preview }
+      end
       collection do 
         get :active
         get :inactive
       end
-      resources :contributions, only: [:new, :create] do
-        collection { get :preview }
-      end
     end
-    resources :users, only: :destroy
   end
-
-  get :membership, to: 'memberships#show'
 
   resources :contributions, only: :index
   resources :distributions, only: :index
   resources :pay_outs, only: :index
   resources :paypalme_handles, only: [:edit, :update]
   resources :currencies, only: [:edit, :update]
-
   resources :dividends, only: [:index, :show] do
     member do 
       patch :pay_out
@@ -35,6 +32,7 @@ Rails.application.routes.draw do
     end 
   end
 
+  get :membership, to: 'memberships#show'
   get :about, to: 'static_pages#about'
   get :mission, to: 'static_pages#mission'
   get :check_email_spam, to: 'static_pages#check_email_spam'
