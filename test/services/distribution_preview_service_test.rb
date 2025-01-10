3f60c7760b1e783603d8e3c4a2da07eb8e0d2ef4
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-01-10 13:23:05 +1000

    Refactor distribution preview service test

--- test/services/distribution_preview_service_test.rb
+++ test/services/distribution_preview_service_test.rb
@@ -2,19 +2,15 @@ require 'test_helper'
 
 class DistributionPreviewServiceTest < ActiveSupport::TestCase
   test 'sends a notification to each user' do
+    ActionMailer::Base.deliveries.clear
+    stub_eu_central_bank_request
     users = [users(:one), users(:two)]
 
-    mailer_mock = mock('mailer')
-    NotificationMailer.stubs(:with).returns(mailer_mock)
-    mailer_mock.stubs(:distribution_preview).returns(mailer_mock)
-    mailer_mock.stubs(:deliver_now).returns(true).times(users.count)
+    DistributionPreviewService.new(users:).call
 
-    assert DistributionPreviewService.new(users:).call
-  end
-
-  test 'all dependent services respond' do
-    stub_eu_central_bank_request
-
-    assert DistributionPreviewService.new(users: [users(:one)]).call
+    assert_equal users.length, ActionMailer::Base.deliveries.count
+    assert(ActionMailer::Base.deliveries.all? do |mail|
+      mail.subject.match?(/Distribution #3 Preview/)
+    end)
   end
 end
