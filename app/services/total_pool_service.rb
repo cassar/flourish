caf2b3cc890f1754ea552ad35d6fec11d1330679
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-01-21 17:51:44 +1000

    Make clear total pool private methods default AUD

--- app/services/total_pool_service.rb
+++ app/services/total_pool_service.rb
@@ -1,7 +1,7 @@
 class TotalPoolService
   class << self
     def balance_in_aud_base_units
-      Integer(total_assets_in_base_units - total_liabilites_in_base_units)
+      Integer(total_assets_in_aud_base_units - total_liabilites_in_aud_base_units)
     end
 
     def balance_formatted(currency)
@@ -13,7 +13,7 @@ class TotalPoolService
     end
 
     def total_contributed_and_recontributed_formatted(currency)
-      amount_in_base_units = total_contributed_in_base_units + total_recontributed_in_base_units
+      amount_in_base_units = total_contributed_in_aud_base_units + total_recontributed_in_aud_base_units
       CurrencyConverter.new(
         from_currency: 'AUD',
         amount_in_base_units:,
@@ -24,7 +24,7 @@ class TotalPoolService
     def total_paid_out_formatted(currency)
       CurrencyConverter.new(
         from_currency: 'AUD',
-        amount_in_base_units: total_paid_out_in_base_units,
+        amount_in_base_units: total_paid_out_in_aud_base_units,
         to_currency: currency
       ).format
     end
@@ -32,46 +32,46 @@ class TotalPoolService
     def total_dividends_formatted(currency)
       CurrencyConverter.new(
         from_currency: 'AUD',
-        amount_in_base_units: total_dividends_in_base_units,
+        amount_in_base_units: total_dividends_in_aud_base_units,
         to_currency: currency
       ).format
     end
 
     private
 
-    def total_assets_in_base_units
-      total_contributed_in_base_units
+    def total_assets_in_aud_base_units
+      total_contributed_in_aud_base_units
     end
 
-    def total_liabilites_in_base_units
+    def total_liabilites_in_aud_base_units
       [
-        total_owed_in_base_units,
-        total_paid_out_in_base_units,
-        total_expense_in_base_units
+        total_owed_in_aud_base_units,
+        total_paid_out_in_aud_base_units,
+        total_expense_in_aud_base_units
       ].sum
     end
 
-    def total_contributed_in_base_units
+    def total_contributed_in_aud_base_units
       in_aud_base_units total_contributions_by_currency
     end
 
-    def total_owed_in_base_units
+    def total_owed_in_aud_base_units
       in_aud_base_units total_owed_dividends_by_currency
     end
 
-    def total_paid_out_in_base_units
+    def total_paid_out_in_aud_base_units
       in_aud_base_units total_paid_out_by_currency
     end
 
-    def total_recontributed_in_base_units
+    def total_recontributed_in_aud_base_units
       in_aud_base_units total_recontributions_by_currency
     end
 
-    def total_dividends_in_base_units
+    def total_dividends_in_aud_base_units
       in_aud_base_units total_dividends_by_currency
     end
 
-    def total_expense_in_base_units
+    def total_expense_in_aud_base_units
       Expense.sum(:amount_in_base_units)
     end
 
