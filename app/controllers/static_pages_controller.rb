94e08723bbebfdda34367a875a55cb6ada3f07b6
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-01-29 10:47:14 +1000

    Add second distribution to show on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @last_distribution = Distribution.last || Distribution.new
-    unless @last_distribution.settled?
-      @last_amount = @last_distribution.default_amount
-      @last_distribution_dividends = @last_distribution.dividends
-    end
+    @last_distribution = Distribution.last || Distribution.new(created_at: Time.zone.now)
+    @last_amount = @last_distribution.default_amount || Amount.new(amount_in_base_units: 0)
+    @last_distribution_dividends = @last_distribution.dividends
     @next_distribution = NextDistributionService
     @total_pool = TotalPoolService
     authenticated_user
