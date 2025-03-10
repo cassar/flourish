079b8682ae4c4d0a88f28ef60585f3f573aa4301
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-03-10 21:42:34 +1000

    Remove old migrations

--- db/migrate/20241113055055_remove_distribution_foreign_key.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDistributionForeignKey < ActiveRecord::Migration[8.1]
-  def change
-    remove_foreign_key :dividends, :distributions
-    change_column_null :dividends, :distribution_id, true
-  end
-end
--- db/migrate/20250302023228_remove_name_column.rb
+++
@@ -1,7 +0,0 @@
-class RemoveNameColumn < ActiveRecord::Migration[8.1]
-  def change
-    remove_column :distributions, :name, :string
-    change_column_null :distributions, :number, false
-    add_index :distributions, :number, unique: true
-  end
-end
