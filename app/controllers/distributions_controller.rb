0c04f56f5f187bc46e953dbef6194d4d43e1e4d0
Luke Cassar <luke.cassar@uqconnect.edu.au>
2025-05-25 15:46:56 +1000

    Add expenses and amounts to distribution page

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -6,7 +6,15 @@ class DistributionsController < ApplicationController
   def show
     @distribution = Distribution.find_by number: params[:id]
     @previous_distribution = Distribution.find_by(number: @distribution.number - 1) || Distribution.new
+    summary_information
+  end
+
+  private
+
+  def summary_information
     @contributions = Contribution.where(created_at: @previous_distribution.created_at..@distribution.created_at)
+    @expenses = Expense.where(created_at: @previous_distribution.created_at..@distribution.created_at)
+    @amounts = @distribution.amounts
     @total_recontributions_formatted = TotalDistributionRecontributionsCalculator
       .new(@previous_distribution)
       .formatted(current_currency)
